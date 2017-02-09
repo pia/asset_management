31734870385c06df9e5c729bbd908a404072b91d
pia <timetopen@gmail.com>
2017-02-09 14:54:18 +0800

    init

---
+++ client-iOS/asset/asset/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  asset
+//
+//  Created by sun on 09/02/2017.
+//  Copyright © 2017 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ client-iOS/asset/asset/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  asset
+//
+//  Created by sun on 09/02/2017.
+//  Copyright © 2017 sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
