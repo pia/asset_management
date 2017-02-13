958d95e434e6b219cca507c665753864aee9b3fb
pia <timetopen@gmail.com>
2017-02-13 13:53:12 +0800

    add tableview in result page

---
+++ client-iOS/asset/asset/ConfigurationTemplate.h
@@ -0,0 +1,16 @@
+//
+//  ConfigurationTemplate.h
+//  asset
+//
+//  Created by Pater Rechards on 2/13/17.
+//  Copyright © 2017 sun. All rights reserved.
+//
+
+#ifndef ConfigurationTemplate_h
+#define ConfigurationTemplate_h
+
+
+#endif /* ConfigurationTemplate_h */
+
+//表格里要显示的标题
+#define titleArray {@"资产名称",@"资产编号",@"购置日期",@"存放地点",@"总价",@"使用人"}
