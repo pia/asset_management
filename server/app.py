e2ea9100c51b6c1b9a688089bf329b58562aaae7
pia <timetopen@gmail.com>
2017-04-17 20:56:17 +0800

    add a demo database

--- server/app.py
+++ server/app.py
@@ -5,31 +5,21 @@ from flask import make_response
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
+def query(task_id):
+    return {}
+    return {"assetName":"book","assetDate":"2016-01-01","assetPlace":"desk","assetPrice":"66    6","    assetUser":"me"}
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@app.route('/asset/<int:task_id>', methods=['GET'])
 def get_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
+    task = query(task_id)
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    else:
+        return jsonify(task)
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({"assetName":"NOT FOUND","assetDate":"NOT FOUND","assetPlace":"NOT FOUND","assetPrice":"NOT FOUND","assetUser":"NOT FOUND"}), 404)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
